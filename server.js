b66111dde955a0111c76e280ae473a9be91abda1
nefims <nefims@gmail.com>
2025-05-22 00:18:29 -0300

    W03 Handling Errors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const static = require("./routes/static");
 const inventoryRoute = require("./routes/inventoryRoute");
 const baseController = require("./controllers/baseController");
-const utilities = require("./utilities");
+const utilities = require("./utilities/");
 
 /* ***********************
  * View Engine and Templates
@@ -31,7 +31,7 @@ app.use(static);
   console.log("View engine:", app.get("view engine"));
   res.render("index", { title: "HomePage" });
 }); */
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome));
 // Inventory routes
 app.use("/inv", inventoryRoute);
 
@@ -41,18 +41,19 @@ app.use(async (req, res, next) => {
 });
 
 /* ***********************
- * Express Error Handler
- * Place after all other middleware
- *************************/
+* Express Error Handler
+* Place after all other middleware
+*************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav();
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
-    title: err.status || "Server Error",
-    message: err.message,
-    nav,
-  });
-});
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
